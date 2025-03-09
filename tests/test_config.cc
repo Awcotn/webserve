23b6d79e64f01f47c7a1c4e7abd640366760da53
Awcotn <1176654839@qq.com>
2025-03-09 18:08:44 +0800

    1

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -3,9 +3,11 @@
 
 awcotn::ConfigVar<int>::ptr g_int_value_config = 
     awcotn::Config::Lookup("system.port", (int)8080, "system port");
+awcotn::ConfigVar<float>::ptr g_float_value_config = 
+    awcotn::Config::Lookup("system.value", (float)10.2f, "system value");
 
 int main(int argc, char** argv) {
     AWCOTN_LOG_INFO(AWCOTN_LOG_ROOT()) << g_int_value_config->getValue();
-    AWCOTN_LOG_INFO(AWCOTN_LOG_ROOT()) << g_int_value_config->toString();
+    AWCOTN_LOG_INFO(AWCOTN_LOG_ROOT()) << g_float_value_config->toString();
     return 0;
 }
